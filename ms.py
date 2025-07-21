da33f10f8f4c7c22d17d17c01e2b0b6096ed42ef
manish <manisharma8591@gmail.com>
2025-07-21 23:40:36 +0530

    name the program

--- ms.py
+++ ms.py
@@ -1,4 +1,5 @@
-# this is my first trial project
+# this is my first trial project just to learn the git and GitHub.
+# add two numbers 
 a = int(input("Enter the first number : "))
 b = int(input("Enter the second number : "))
 c =(a + b)
